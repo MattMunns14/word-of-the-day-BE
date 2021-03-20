9af28005ba8b667da0a41bb2f53112e1091cb9e0
Matt Munns <matthewmunns0@gmail.com>
2021-03-20 16:10:37 -0400

    hi

--- index_operations/app.py
+++ index_operations/app.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "workflow test",
+            "message": "Hi Erin!",
         }),
     }
 
