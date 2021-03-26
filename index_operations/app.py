e20e7298852af8377ee126b4a5eb6ea65a68d809
Matt Munns <matthewmunns0@gmail.com>
2021-03-26 19:05:23 -0400

    more headers

--- index_operations/app.py
+++ index_operations/app.py
@@ -5,6 +5,11 @@ def lambda_handler(event, context):
     print(event)
 
     return {
+        "headers": {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': 'https://wordsoftheday.org',
+            'Access-Control-Allow-Methods': 'POST,GET'
+        },
         "statusCode": 200,
         "body": json.dumps({
             "message": "Hi Erin!",
